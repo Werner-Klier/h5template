d018af157198ec9a8cd3ddca28624e4e6fcb5156
刘相成 <2307110246@qq.com>
2020-11-04 14:35:48 +0800

    添加loading

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <Loading v-if="$store.state.LOADING"></Loading>
   </div>
 </template>
 
+<script>
+import Loading from "@/components/loading"
+export default {
+  components:{
+    Loading
+  }
+};
+</script>
+
 <style lang="less">
-#app{
-  *{
+#app {
+  * {
     box-sizing: border-box;
   }
   width: 100%;
   height: 100%;
   // min-width: 375px;
-  &>div{
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column
+  & > div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
   }
 }
 </style>
---
+++ src/components/loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading">
+    <van-loading type="spinner" color="#1989fa" />
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import {Loading} from 'vant';
+
+  Vue.use(Loading);
+
+  export default {
+    name: 'LOADING',
+    data() {
+      return {}
+    },
+  }
+</script>
+<style lang="less" scoped>
+  .loading {
+    position: fixed;
+    z-index: 9999;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    background: rgba(255, 255, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
