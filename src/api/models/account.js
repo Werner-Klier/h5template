e9d1dec17820770cdc938b5699c0f79c99889f31
mir-lxc <57943259+mir-lxc@users.noreply.github.com>
2020-11-30 15:23:20 +0800

    Update account.js

--- src/api/models/account.js
+++ src/api/models/account.js
@@ -2,5 +2,5 @@
 import http from "@/utils/request.js";
 export default {
     // 获取用户认证的状态基本信息
-  getStateInfo: (params = {}) => http.get("", params),
+  getStateInfo: (params = {}) => http.get("", params,{loading: true}),
 };
